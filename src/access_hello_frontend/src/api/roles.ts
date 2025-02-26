7f3c5e894ea938887622ddb22ccdd7bbdf5ef35c
552020 <stefanolombardo@posteo.de>
2025-02-26 22:21:25 +0100

    chore: add auto assignemnt at login

--- src/access_hello_frontend/src/api/roles.ts
+++ src/access_hello_frontend/src/api/roles.ts
@@ -1,5 +1,7 @@
 import { access_hello_backend } from "../../../declarations/access_hello_backend";
 import { Role } from "../../../declarations/access_hello_backend/access_hello_backend.did";
+import { AuthClient } from "@dfinity/auth-client";
+import { login } from "./auth";
 
 export const getCurrentRole = async (): Promise<Role[]> => {
   const roles = await access_hello_backend.get_caller_role();
@@ -20,4 +22,14 @@ export const whoAmI = async () => {
 
 export const greet = async (name: string) => {
   return await access_hello_backend.greet_alt(name);
+};
+
+export const autoAssignUserRole = async () => {
+  return await access_hello_backend.auto_assign_user_role();
+};
+
+// New function that combines login and role assignment
+export const autoAssignUserRoleAtLogin = async (authClient: AuthClient) => {
+  await login(authClient);
+  await autoAssignUserRole();
 };
